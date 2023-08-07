22cac3a12fab41dc4e9bb5701558345277a929d3
mordechai-dror <mordechai.dror@lendai.us>
2023-08-07 15:52:59 +0300

    cdk fixes

--- projects/lendai-us/cdk/src/lib/pipes/at.pipe.ts
+++ projects/lendai-us/cdk/src/lib/pipes/at.pipe.ts
@@ -1,12 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { at } from 'lodash';
+import { Dictionary, NumericDictionary, PropertyPath, at } from 'lodash';
 
 @Pipe({
   name: 'at',
   standalone: true,
 })
 export class AtPipe implements PipeTransform {
-  transform(...params: Parameters<typeof at>): ReturnType<typeof at> {
-    return at(...params);
+  transform<T>(
+    object: Dictionary<T> | NumericDictionary<T> | null | undefined,
+    ...props: PropertyPath[]
+  ): T[] {
+    return at(object, ...props);
   }
 }
