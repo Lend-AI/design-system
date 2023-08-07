cbdc57fe1ae405254be30ec6f04d5860c964935e
mordechai-dror <mordechai.dror@lendai.us>
2023-08-07 11:18:12 +0300

    ng g lib @lendai/design-system --prefix lai

---
+++ projects/lendai/design-system/src/lib/design-system.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lai-design-system',
+  template: `
+    <p>
+      design-system works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class DesignSystemComponent {
+
+}
