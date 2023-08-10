5a636718d1ce338c6206f3946129400e57dd7d09
Nofar Shlomo <nofar.shlomo@lendai.us>
2023-08-10 17:47:09 +0300

    landing-page-layout-footer component

---
+++ projects/lendai-us/design-system/src/lib/layout/landing-page-layout/landing-page-layout-footer/landing-page-layout-footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lai-landing-page-layout-footer',
+  templateUrl: './landing-page-layout-footer.component.html',
+  styleUrls: ['./landing-page-layout-footer.component.scss'],
+})
+export class LandingPageLayoutFooterComponent {}
