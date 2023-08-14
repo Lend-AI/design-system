b69e8f27bc14644b39f73142f6eb8d3f4e8df166
Nofar Shlomo <nofar.shlomo@lendai.us>
2023-08-14 15:41:52 +0300

    remove landing-page-layout-logo component

--- projects/lendai-us/design-system/src/lib/layout/landing-page-layout/index.ts
+++ projects/lendai-us/design-system/src/lib/layout/landing-page-layout/index.ts
@@ -1,5 +1,4 @@
 export * from './landing-page-layout-actions/landing-page-layout-actions.component';
-export * from './landing-page-layout-logo/landing-page-layout-logo.component';
 export * from './landing-page-layout-categories/landing-page-layout-categories.component';
 export * from './landing-page-layout-content/landing-page-layout-content.component';
 export * from './landing-page-layout-footer/landing-page-layout-footer.component';
