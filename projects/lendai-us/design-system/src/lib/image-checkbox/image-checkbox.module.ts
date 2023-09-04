cd4d35f48091eacb8d6ef129841adbebdefc8fd4
mordechai-dror <mordechai.dror@lendai.us>
2023-09-04 16:42:26 +0300

    0.0.19

--- projects/lendai-us/design-system/src/lib/image-checkbox/image-checkbox.module.ts
+++ projects/lendai-us/design-system/src/lib/image-checkbox/image-checkbox.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImageCheckboxGroupComponent } from './image-checkbox-group/image-checkbox-group.component';
 import { ImageCheckboxComponent } from './image-checkbox/image-checkbox.component';
-import { LabelComponent } from '../typography';
+import { TitleComponent } from '../typography';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [ImageCheckboxGroupComponent, ImageCheckboxComponent],
-  imports: [CommonModule, LabelComponent, MatCheckboxModule],
+  imports: [CommonModule, TitleComponent, MatCheckboxModule],
   exports: [ImageCheckboxGroupComponent, ImageCheckboxComponent],
 })
 export class ImageCheckboxModule {}
