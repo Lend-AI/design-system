e8304d26284ef4ec47aab0b61fa53205d1e911e0
mordechai-dror <mordechai.dror@lendai.us>
2023-10-11 12:36:40 +0300

    fix clean layout service exports

--- projects/lendai-us/design-system/src/lib/layout/clean-layout/index.ts
+++ projects/lendai-us/design-system/src/lib/layout/clean-layout/index.ts
@@ -2,3 +2,4 @@ export * from './clean-layout-left/clean-layout-left.component';
 export * from './clean-layout-right/clean-layout-right.component';
 export * from './clean-layout.component';
 export * from './clean-layout.module';
+export * from './clean-layout.service';
